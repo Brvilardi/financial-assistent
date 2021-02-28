0adc13a5e7a9d06c8f96beb2cc2d85ed65128fa4
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-02-28 16:44:37 -0300

    RDS db integration

--- planejamentoFinanceiro/settings.py
+++ planejamentoFinanceiro/settings.py
@@ -87,13 +87,22 @@ WSGI_APPLICATION = 'planejamentoFinanceiro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': customScripts.getVariableData(CREDENTIALS_PATH, "DATABASE_NAME"),
+        'USER': customScripts.getVariableData(CREDENTIALS_PATH, "DATABASE_USER"),
+        'PASSWORD': customScripts.getVariableData(CREDENTIALS_PATH, "DATABASE_PASSWORD"),
+        'HOST': customScripts.getVariableData(CREDENTIALS_PATH, "DATABASE_HOST"),
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
